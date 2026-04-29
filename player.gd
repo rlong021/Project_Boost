8e85a256568ea116fb1f1d82248016a8cef0180a
Rogelio Long <rlong021@gmail.com>
2026-04-28 21:46:43 -0600

    move player

--- player.gd
+++ player.gd
@@ -1,11 +1,22 @@
-extends Node3D
+extends RigidBody3D
+
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * 1000)
 	
-	if Input. is_action_pressed("ui_left"):
-		rotate_z(delta)
-	if Input. is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input. is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0.0,0.0,100.00 * delta))
+		
+	if Input. is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0,0.0,-100.00 * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Goal"):
+		print("You win!")
+		
+	if body.is_in_group("Hazard"):
+		print("You crashed!")
+	print(body.name)
