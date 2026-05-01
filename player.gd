aa1868185f21a57a54d415e417e0a420a3069fa4
Rogelio Long <rlong021@gmail.com>
2026-05-01 15:30:46 -0600

    update

--- player.gd
+++ player.gd
@@ -1,22 +1,35 @@
 extends RigidBody3D
 
+## How much vercial force to apply when moving
+@export_range(750.0,3000.0) var thrust: float = 1000.0
+
+@export var torque_thrust: float = 100.0
 
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000)
+		apply_central_force(basis.y * delta * thrust)
 	
 	if Input. is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0,0.0,100.00 * delta))
+		apply_torque(Vector3(0.0,0.0,torque_thrust * delta))
 		
 	if Input. is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0,0.0,-100.00 * delta))
+		apply_torque(Vector3(0.0,0.0,-torque_thrust * delta))
 
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("Goal"):
+		complete_level()
 		print("You win!")
 		
 	if body.is_in_group("Hazard"):
-		print("You crashed!")
+		crash_sequence()
 	print(body.name)
+
+func crash_sequence() -> void:
+	print("Kaboom!")
+	get_tree().reload_current_scene()
+	
+func complete_level() -> void:
+	print("You win!")
+	get_tree().quit()
